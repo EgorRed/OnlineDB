c70d0b38962f069884559898ed5f28c6b938967f
EgorRed <egor.red02@gmail.com>
2024-07-08 19:35:28 +0300

    add service UserFileDataManagerService01

--- FileManager/Services/UserFileDataManagerService.cs
+++ FileManager/Services/UserFileDataManagerService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FileManager.interfaces;
+using FileManager.Models.UserFileDataModels;
+using Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,36 @@ using System.Threading.Tasks;
 
 namespace FileManager.Services
 {
-    public class UserFileDataManagerService
+    public class UserFileDataManagerService : IUserFileDataManager
     {
+        public Task<Status> CreateDataFile(CreateDataFileModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Status> CreateUserFolder(CreateUserFolderModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Status> DeleteDataFile(DeleteDataFileModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Status> DeleteUserFolder(DeleteUserFolderModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Status> UpdateDataFile(UpdateDataFileModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Status> UpdateUserFolder(UpdateUserFolderModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
